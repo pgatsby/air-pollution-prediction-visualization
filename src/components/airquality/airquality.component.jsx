b3cf3c6e7a8d24a07b22873aebd7b0bfaf676902
pgatsby <dev@pgatsby.com>
2021-02-07 22:44:58 -0800

    added home page header for air-quality

---
+++ src/components/airquality/airquality.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./airquality.style.scss";
+
+class AirQuality extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    handleSubmit = (event) => {
+        console.log("we made it here")
+        event.preventDefault();
+    }
+
+  render() {
+    return (
+      <div className="air-quality container w-100">
+        <div className="row pt-4 pb-4">
+          <div className="col-12 col-lg-6 pb-4">
+            <h1 className="align-self-center" id="aq-title">
+              LOS ANGELES <br />
+              AIR QUALITY
+            </h1>
+            <button className="btn btn-dark" id="explore">
+              Explore <i className="fas fa-arrow-right"></i>
+            </button>
+          </div>
+          <div className="col-12 col-lg-6 pb-4">
+            <form onSubmit={this.handleSubmit}>
+              <label className="form-label">Find Your City!</label>
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="Enter Zip Code Here"
+                id="aq-lookup"
+              />
+            </form>
+          </div>
+        </div>
+        <h1 className="text-center">Current Forcast For Los Angeles</h1>
+        <div className="row pt-4 pb-4"></div>
+      </div>
+    );
+  }
+}
+export default AirQuality;
--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-import Map from "../../components/map/map.component.jsx";
+import AirQuality from "../../components/airquality/airquality.component.jsx";
 import MapForm from "../../components/mapform/mapform.component.jsx";
+import Map from "../../components/map/map.component.jsx";
 import "./homepage.style.scss";
 
 class HomePage extends React.Component {
@@ -23,15 +24,21 @@ class HomePage extends React.Component {
     });
   };
 
+  scrollUp(){
+    window.scrollTo(0,0);
+  }
+
   render() {
     return (
       <div className="home-page">
+        <AirQuality />
         <MapForm
           id={this.state.id}
           handleSubmit={this.handleSubmit}
           handleClick={this.handleClick}
         />
         <Map id={this.state.id} />
+        <div className="home-page-footer"><button className="btn btn-dark" id="scroll-up" onClick={this.scrollUp}><i className="fas fa-chevron-double-up"></i></button></div>
       </div>
     );
   }
