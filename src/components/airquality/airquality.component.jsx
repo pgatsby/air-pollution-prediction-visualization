abe52af3da677aa35fe05a0fffec1d2bcc76e4a6
pgatsby <dev@pgatsby.com>
2021-02-08 02:22:54 -0800

    added Weatherbit API to retrieve current AQI and weather data

--- src/components/airquality/airquality.component.jsx
+++ src/components/airquality/airquality.component.jsx
@@ -3,14 +3,46 @@ import React from "react";
 import "./airquality.style.scss";
 
 class AirQuality extends React.Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+    this.url = "https://api.weatherbit.io/v2.0/current?postal_code=";
+    this.key = "fcdc42ccf3e84edbb3f31b586099d417";
+    this.state = {
+      postal_code: "90037",
+      cityName: null,
+      stateCode: null,
+      aqiCode: null,
+      weatherCode: null,
+      weatherIcon: null,
+    };
+  }
 
-    handleSubmit = (event) => {
-        console.log("we made it here")
-        event.preventDefault();
-    }
+  retrieveData(postal_code) {
+    fetch(this.url + this.state.postal_code + "&key=" + this.key)
+      .then((response) => response.json())
+      .then(({ data }) => {
+        console.log(data[0]);
+        console.log(data[0].aqi);
+        console.log(data[0].weather.code);
+        this.setState({
+          cityName: data[0].city_name,
+          stateCode: data[0].state_code,
+          aqiCode: data[0].aqi,
+          weatherCode: data[0].weather.code,
+          weatherIcon: data[0].weather.icon,
+        });
+        console.log(this.state);
+      });
+  }
+
+  componentWillMount() {
+    this.retrieveData();
+  }
+
+  handleSubmit = (event) => {
+    console.log("we made it here");
+    event.preventDefault();
+  };
 
   render() {
     return (
@@ -35,10 +67,17 @@ class AirQuality extends React.Component {
                 id="aq-lookup"
               />
             </form>
+            <div className="aq-details mt-3">
+              <h2>Current Air Quality</h2>
+              <div className="aq-d-location">
+                <strong>{this.state.cityName}, {this.state.stateCode}</strong>
+              </div>
+              <div className="aq-d-aqi">
+                AQI: {this.state.aqiCode} 
+              </div>
+            </div>
           </div>
         </div>
-        
-        <div className="row pt-4 pb-4"></div>
       </div>
     );
   }
