842f2ee0381ac7afed1e35dc8601c2ebb572d243
eric436chan <eric436.chan@gmail.com>
2021-02-14 20:13:11 -0800

    added initial dashboard

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import AirQuality from "../../components/airquality/airquality.component.jsx";
 import MapForm from "../../components/mapform/mapform.component.jsx";
 import Map from "../../components/map/map.component.jsx";
+import PieChart from "../../components/graphs/piechart.component.jsx";
 import "./homepage.style.scss";
+
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -47,6 +49,31 @@ class HomePage extends React.Component {
             <Map id={this.state.id} />
           </div>
         </div>
+        <div class='graph-container dashboard'>
+          <PieChart title="Greenhouse Gas Emissions By Sectors" dataPoints={
+            [
+              { y: 21, label: "Industry" },
+              { y: 14, label: "Transport" },
+              { y: 35, label: "Energy" },
+              { y: 6, label: "Buildings" },
+              { y: 24, label: "Agriculture" }
+            ]
+          }/>
+          <br/>
+          <PieChart title="Deaths Caused by Ambient Air Pollution" dataPoints={
+            [
+              { y: 40, label: "Ischemic Heart Disease"},
+              { y: 40, label: "Stroke"},
+              { y: 11, label: "Chronic Obstructive Pulmonary Disease"},
+              { y: 6, label: "Lung Cancer"},
+              { y: 3, label: "Acute Lower Respiratory Infections in Children"}
+            ]
+          }/>
+        </div>
+        <div class='article-container dashboard'>
+          Articles go here
+        </div>
+       
       </div>
     );
   }
